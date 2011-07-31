d0307ad18644c08249b1cdb01c818d03bbd89c14
Roberto Vasquez Angel <robotex@robotex.de>
2011-07-31 03:08:16 +0200

    first commit

---
+++ lib/ecm_contact_form.rb
@@ -0,0 +1,2 @@
+module EcmContactForm
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '4d4c66c5b38943cd5e2534fb1004349fd5f13954da817f2f91d16f4ac7227eff7cb2ebaef22d9d6af05e4208c75d295e73e5bc898bf93f8fbfd53a72218a4a44'
